39184ae1683910b39a1385782b80b9da2d770d08
unknown <1176304452@qq.com>
2020-07-05 10:36:23 +0800

    yunnan_init

---
+++ test_demo/git.md
@@ -0,0 +1,3 @@
+# git使用命令练习
+## 关于远程仓库的操作
+1. 在远程仓库创建分支的命令为 git branch --set-upstream origin/dev
---
+++ yunnan/yunnan/src/route/home/index.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        path: '/home',
+        name: 'home',
+        meta: {
+            title: '首页',
+            handShare: true
+        },
+        component: () => {
+            import(
+                
+            )
+        }
+    }
+]
